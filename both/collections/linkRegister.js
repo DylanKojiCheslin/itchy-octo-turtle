bc423ce92c9cca0442fbf4d2987fd7844a84b89f
dylan <dylan.kojicheslin@gmail.com>
2015-11-14 03:45:41 -0700

    added students linking for later use

--- both/collections/linkRegister.js
+++ both/collections/linkRegister.js
@@ -1,3 +1,6 @@
 
 Classes.registerLinkableType(Students, "student");
 configureLinkableType(Classes, Students, "student");
+
+// Classes.registerLinkableType(Classes, "classes");
+// configureLinkableType(Students, Classes, "classes");
