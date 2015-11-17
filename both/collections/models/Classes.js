cc5aa4efe214b765c120cb5761854fd183caf9f6
dylan <dylan.kojicheslin@gmail.com>
2015-11-16 18:27:40 -0700

    added listStudents method to class

--- both/collections/models/Classes.js
+++ both/collections/models/Classes.js
@@ -7,14 +7,31 @@ Classes.appendSchema({
       type: String,
       max: 20
    },
-
+   "time":{
+     type: String,
+     max: 30
+   }
 });
 
 Classes.methods({
 	"className": function(){
-		return this.title;
+		return this.time + " " + this.title;
 	},
 	"classSlug": function(){
 		return this._id;
 	},
+  "listStudents": function(){
+    var studentsIdsArray = [];
+    var seachSelector = {};
+    var studentsCurssor = {};
+    var studentsArray = [];
+    var getStudentsFullName = function(element){
+      return element.fullName()
+    };
+    studentsIdsArray = this.links.student;
+    seachSelector._id = { $in: studentsIdsArray};
+    studentsCurssor = StudentsCollection.find(seachSelector);
+    studentsArray = studentsCurssor.map(getStudentsFullName);
+    return studentsArray;
+  }
 });
